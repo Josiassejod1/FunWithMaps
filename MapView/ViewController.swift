9d3dcab8cc453b087db5d762dc2fab20ba481369
Dalvin <dalvin@Dalvins-MBP.fios-router.home>
2019-01-14 23:06:19 -0500

    learn how to use maps

--- MapView/ViewController.swift
+++ MapView/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    @IBOutlet var Map: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        var location = CLLocationCoordinate2DMake(40.706268, -74.176811)
+        
+    
+        var span = MKCoordinateSpan.init(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        
+        var region = MKCoordinateRegion(center:location, span: span)
+        Map.setRegion(region, animated: true)
+        
+        var annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "Newark Airport"
+        annotation.subtitle = "Terminal C"
+        Map.addAnnotation(annotation)
+        
+       
     }
 
 
